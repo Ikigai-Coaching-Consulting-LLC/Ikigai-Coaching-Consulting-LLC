5002c3b3d80b569a4e5141231fd681d6e716c899
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-09-27 15:24:29 -0400

    fix tsconfig.json + Components imports in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
 import React from "react"
-import { Section } from "../components/Section/Section";
-import { Wrapper } from "../components/Wrapper/Wrapper.style";
-import { ChatBot } from "../components/ChatBot/ChatBot";
-import { ContactForm } from "../components/ContactForm/ContactForm";
-import Navbar from "../Components/Navbar"
 import { Section } from "../Components/Section/Section";
 import { ChatBot } from "../Components/ChatBot/ChatBot";
+import { ContactForm } from "../Components/ContactForm/ContactForm";
+import Navbar from "../Components/Navbar"
 
 //Styling
 import { Wrapper } from "../styles/Home.styles";
