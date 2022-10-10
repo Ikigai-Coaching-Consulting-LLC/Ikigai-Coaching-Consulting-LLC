d5e9b2f4832d610733a21c0a18c3291df7919be5
Austin <austin.dober@gmail.com>
2022-10-10 01:01:05 -0400

    removing stylingJson and adding title on heroimage

--- Components/HeroImage/HeroImage.styles.tsx
+++ Components/HeroImage/HeroImage.styles.tsx
@@ -6,16 +6,16 @@ export interface Props {
 
 export const Wrapper = styled.div<Props> ` 
     width: 100vw;
-    height: 80vh;
+    height: 100vh;
     border: '1px solid black';
-    background: linear-gradient(90deg, rgba(0,0,0,0.2) 25%, rgba(0,0,0,0.8) 86.6%), url(${(props:Props) => props.background ? props.background: ''}), lightgrey;
-    background-size:cover ;
+    background: linear-gradient(90deg, rgba(0,0,0,0.4) 25%, rgba(0,0,0,0.8) 86.6%), url(${(props:Props) => props.background ? props.background: ''}), lightgrey;
+    background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
     padding: 10px 20px;
 `;
 
-export const Image = styled.img` 
+export const Title = styled.h1 ` 
 
 
 `
--- Components/HeroImage/HeroImage.tsx
+++ Components/HeroImage/HeroImage.tsx
@@ -1,13 +1,15 @@
 
-import { Wrapper, Image } from './HeroImage.styles'
+import { Wrapper, Title } from './HeroImage.styles'
 
 export interface HeroImageProps {
     background?: string
+    title?: string
 }
 
 const HeroImage = (props:HeroImageProps) => {
     return (
         <Wrapper background={props.background}>
+            <Title></Title>
             
         </Wrapper>
     )
